630764cf38dd5748eca1ee8f01d760244e423f06
BilousOleg <olezhka82802@gmail.com>
2026-01-02 18:31:52 +0200

    Add second exercise solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,14 @@
-const lowerNumberLimit = 1;
-const upperNumberLimit = 10;
+const PASSWORD_PATTERN = 'zxcvbn';
+let isPasswordCorrect = false; // Винесено в окрему змінну для покращення читабельності коду та уникнення повторень
+let isCancelPressed = false; // Винесено в окрему змінну для покращення читабельності коду та уникнення повторень
 
-// for, оскільки ми фактично ПЕРЕБИРАЄМО проміжок від lowerNumberLimit до upperNumberLimit
-for (let i = lowerNumberLimit; i <= upperNumberLimit; i++) {
-  console.log(i * i);
+// Цикл з постумовою для того, щоб перевіряти вже отримані дані від користувача
+do {
+  let password = prompt('Введіть пароль');
+  isPasswordCorrect = password === PASSWORD_PATTERN;
+  isCancelPressed = password === null;
+} while (!isPasswordCorrect && !isCancelPressed);
+
+if (isPasswordCorrect && !isCancelPressed) {
+  console.log('Ви успішно увійшли в систему');
 }
