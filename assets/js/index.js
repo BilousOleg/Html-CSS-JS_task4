fc5f22caf602b8d01e42378270b48cf33c144b68
BilousOleg <olezhka82802@gmail.com>
2026-01-03 01:05:18 +0200

    Minor changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
+// Завдання 1
+
+const lowerNumberLimit = 1;
+const upperNumberLimit = 10;
+
+// for, оскільки ми фактично ПЕРЕБИРАЄМО проміжок від lowerNumberLimit до upperNumberLimit
+for (let i = lowerNumberLimit; i <= upperNumberLimit; i++) {
+  console.log(i * i);
+}
+
+// Завдання 2
+
 const PASSWORD_PATTERN = 'zxcvbn';
 let isPasswordCorrect = false; // Винесено в окрему змінну для покращення читабельності коду та уникнення повторень
 let isCancelPressed = false; // Винесено в окрему змінну для покращення читабельності коду та уникнення повторень
