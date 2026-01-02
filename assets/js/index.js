4f95959b74cefda6f52f03524a0820b485c86187
BilousOleg <olezhka82802@gmail.com>
2026-01-02 16:58:06 +0200

    Add first exercise solution

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const lowerNumberLimit = 1;
+const upperNumberLimit = 10;
+
+// for, оскільки ми фактично ПЕРЕБИРАЄМО проміжок від lowerNumberLimit до upperNumberLimit
+for (let i = lowerNumberLimit; i <= upperNumberLimit; i++) {
+  console.log(i * i);
+}
