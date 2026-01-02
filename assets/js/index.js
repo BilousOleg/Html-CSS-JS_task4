251585691fd422f263831dc18a76a4d0e34cffa8
BilousOleg <olezhka82802@gmail.com>
2026-01-02 20:04:06 +0200

    Change variables names

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,10 @@
-const lowerNumberLimit = 1;
-const upperNumberLimit = 100;
+const lowerLimit = 1;
+const upperLimit = 100;
 let sum = 0; // Змінна акумулятор
 
-// Цикл for, адже ми фактично перебираємо проміжок чисел від lowerNumberLimit до upperNumberLimit
-for (let i = lowerNumberLimit; i <= upperNumberLimit; i++) {
+// Цикл for, адже ми фактично перебираємо проміжок чисел від lowerLimit до upperLimit
+for (let i = lowerLimit; i <= upperLimit; i++) {
   sum += i;
 }
 
-console.log(
-  `Сума чисел від ${lowerNumberLimit} до ${upperNumberLimit} = ${sum}`
-);
+console.log(`Сума чисел від ${lowerLimit} до ${upperLimit} = ${sum}`);
