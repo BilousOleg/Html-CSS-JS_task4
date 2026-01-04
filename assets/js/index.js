4cefe55e5a1b6c817c7a0b2f5d18246a6635e67f
Bilous Oleg <olezhka82802@gmail.com>
2026-01-04 19:26:54 +0200

    Improve code readability

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,11 @@ const PASSWORD_PATTERN = 'zxcvbn';
 let isPasswordCorrect = false; // Винесено в окрему змінну для покращення читабельності коду та уникнення повторень
 let isCancelPressed = false; // Винесено в окрему змінну для покращення читабельності коду та уникнення повторень
 
-// Цикл з постумовою для того, щоб перевіряти вже отримані дані від користувача
-do {
+while (!isPasswordCorrect && !isCancelPressed) {
   let password = prompt('Введіть пароль');
   isPasswordCorrect = password === PASSWORD_PATTERN;
   isCancelPressed = password === null;
-} while (!isPasswordCorrect && !isCancelPressed);
+}
 
 if (isPasswordCorrect && !isCancelPressed) {
   console.log('Ви успішно увійшли в систему');
